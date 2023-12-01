fa6bf1697f44f6b3a49453d1f88bb3d417ca8239
Deepak Gathwal <rickygathwal@gmail.com>
2023-12-02 00:21:41 +0530

    Update URL.jsx
    db

--- client/src/CommonUrl/URL.jsx
+++ client/src/CommonUrl/URL.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const instance =  axios.create({baseURL:"http://localhost:8080/memories/"})
+export const instance =  axios.create({baseURL:"https://comment-world.onrender.com/memories/"})
 instance.defaults.headers.common['Content-Type'] = 'multipart/form-data';
 instance.defaults.withCredentials = true;
 
-export const imgLink = `http://localhost:8080`;
+export const imgLink = `https://comment-world.onrender.com`;
 
 
