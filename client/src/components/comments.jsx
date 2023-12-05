98f385a5b08cff64ec9799b28d23ddba352cb43f
Deepak Gathwal <rickygathwal@gmail.com>
2023-12-05 12:26:32 +0530

    Update comments.jsx

--- client/src/components/comments.jsx
+++ client/src/components/comments.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
+import { ImReply } from "react-icons/im";
 
-import { imgLink, instance } from '../CommonUrl/URL'
-
+import { imgLink } from '../CommonUrl/URL'
+// show comment on post 
 const Comments = ({ commentBlock,  el,addComment,setAllComments }) => {
 
   const [edit,setEdit]= useState(-1)
@@ -21,36 +22,41 @@ const Comments = ({ commentBlock,  el,addComment,setAllComments }) => {
            
             <>
               <div className="col-4">
-    <img className='m-2' src={`${imgLink}${ab.userId.image}`} alt='User Profile Pic' style={{borderRadius:"50%", height:"35px", width:'35px'}}/> 
-                <h6>
-                  {" "}
-     
-                  <b>{ab.userId.fullName}</b>{" "}
-                </h6>
+    <img className='m-1' src={`${imgLink}${ab.userId.image}`} alt='User Profile Pic' style={{borderRadius:"50%", height:"30px", width:'30px'}}/> 
+                <span className="fw-semibold">
+                
+                {ab.userId.fullName}
+                </span>
               </div>
                 <div className="col-4">
-                <h6>{ab.comment}</h6>
+                <p className="mt-1">{ab.comment}</p>
 
                 </div>
-                <div className="col-4">
-                <h6 onClick={(e) => openCommentBox(ab._id)}>Replay</h6>
+                <div className="col-4" onClick={(e) => openCommentBox(ab._id)}>
+                <span title="Add a reply off this comment" className="mt-1" >  <ImReply /></span>
 
                 </div>
                 {edit == ab._id &&
                   <>
 
+                
                   <form action="">
-                  <input
-                  type="text"
-                  placeholder="comment"
-                  onChange={(e) => setAllComments(e.target.value)}
-                />
-                <input
-                  type="submit"
-                  onClick={(e, i) => addComment(el, ab.commentId)}
-                />
+      <div className=" d-flex flex-column m-auto">
+      <input
+       type="text"
+       className='m-auto me-0  border text-right rounded-2'
+       placeholder="reply  comment"
+       onChange={(e) => setAllComments(e.target.value)}
+     />
+     <input
+     value="Reply"
+       type="submit"
+       className='m-auto me-0 mt-2 border rounded bg-light fw-semibold'
+       onClick={(e, i) => addComment(el, ab.commentId)}
+     />
 
-                  </form>
+      </div>
+      </form>
                 </>
                 }
             
